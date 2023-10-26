091b3cd75cdbeb44f348d751d730cb13c439138f
Satwik Kambham <satwik.kambham@gmail.com>
2023-10-26 11:40:44 +0530

    Added tailwind

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
-const EleventyVitePlugin = require("@11ty/eleventy-plugin-vite");
-
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPlugin(EleventyVitePlugin);
+    eleventyConfig.addPassthroughCopy({
+        "./public": "./"
+    });
 
     return {
 
