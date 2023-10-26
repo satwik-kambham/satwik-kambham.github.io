77dff0d001acce25a5b6bf477c9fd5221d3288b9
Satwik Kambham <satwik.kambham@gmail.com>
2023-10-26 11:13:01 +0530

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,9 @@
+const EleventyVitePlugin = require("@11ty/eleventy-plugin-vite");
+
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPlugin(EleventyVitePlugin);
+
+    return {
+
+    }
+}
